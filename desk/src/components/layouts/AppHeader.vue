2bb05d8fc08594b833007b21bea33527d1757bca
Robi Tejo Surasko <robisurasko@gmail.com>
2025-06-04 13:51:08 +0700

    Update for navigations on local
    update status in navigations

--- desk/src/components/layouts/AppHeader.vue
+++ desk/src/components/layouts/AppHeader.vue
@@ -1,16 +1,14 @@
 <template>
-<<<<<<< Updated upstream
+
   <div class="flex border-b pr-5">
     <div id="app-header" class="flex-1 w-full"></div>
   </div>
 </template>
 
 <script setup></script>
-=======
   <div class="border-b">
     <div id="app-header" class="w-full flex items-center justify-center px-2 py-1">
       </div>
     </div>
   </div>
 </template>
->>>>>>> Stashed changes
